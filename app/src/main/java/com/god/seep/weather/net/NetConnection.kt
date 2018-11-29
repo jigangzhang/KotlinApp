d489f32d07d298e919c7e5d3ea2c834ccc588401
jigangzhang <1045304065@qq.com>
2018-11-29 17:22:34 +0800

    NetConnection优化--新增一些状态；

--- app/src/main/java/com/god/seep/weather/net/NetConnection.kt
+++ app/src/main/java/com/god/seep/weather/net/NetConnection.kt
@@ -1,5 +1,6 @@
 package com.god.seep.weather.net
 
+import java.io.File
 import java.io.InputStream
 import java.io.OutputStream
 import java.lang.Exception
@@ -7,13 +8,24 @@ import java.net.InetSocketAddress
 import java.net.Socket
 
 class NetConnection {
+    companion object {
+        val COMMAND_REV_FILE: String = "command_rev_file"
+        val COMMAND_SEND_FILE: String = "command_send_file"
+        val COMMAND_DENY: String = "command_deny"
+        val COMMAND_ACCEPT: String = "command_accept"
+    }
+
     private val host: String = "10.0.0.12"
     private val port: Int = 9999
     private var socket: Socket? = null
+    private var mInputStream: InputStream? = null
+    private var mOutputStream: OutputStream? = null
 
     init {
         try {
             socket = Socket(host, port)
+            mInputStream = socket?.getInputStream()
+            mOutputStream = socket?.getOutputStream()
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -21,6 +33,8 @@ class NetConnection {
 
     fun reConnect() {
         socket?.connect(InetSocketAddress(host, port))
+        mInputStream = socket?.getInputStream()
+        mOutputStream = socket?.getOutputStream()
     }
 
     fun getInputStream(): InputStream? = socket?.getInputStream()
@@ -28,6 +42,25 @@ class NetConnection {
     fun getOutputStream(): OutputStream? = socket?.getOutputStream()
 
     fun isConnected(): Boolean = socket?.isConnected ?: false
+
+    fun readCommand(): String = mInputStream?.readBytes().toString()
+
+    fun writeCommand(command: String) {
+        mOutputStream?.write(command.toByteArray())
+        mOutputStream?.flush()
+    }
+
+    fun sendFile(file: File) {
+        mOutputStream?.write(file.inputStream().read())
+    }
+
+    fun saveFile(fileName: String) {
+        if (mInputStream != null) {
+            val fos = File(fileName).outputStream()
+            fos.write(mInputStream!!.read())
+            fos.flush()
+        }
+    }
 
     fun close() = socket?.close()
 }
--- app/src/main/java/com/god/seep/weather/ui/TransportActivity.kt
+++ app/src/main/java/com/god/seep/weather/ui/TransportActivity.kt
@@ -5,15 +5,52 @@ import android.os.Bundle
 import android.util.Log
 import com.god.seep.weather.R
 import com.god.seep.weather.net.NetConnection
+import java.io.*
+import java.lang.Exception
 
+/**
+ * 文件  C <--> S (相互传)
+ * 消息  线程间交互
+ */
 class TransportActivity : AppCompatActivity() {
+    private var connection: NetConnection? = null
+    private var disconnect: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transport)
-        Thread {
-            val connection = NetConnection()
-            Log.e("tag", "connect-->" + connection.isConnected())
+        Thread {//service？？
+            connection = NetConnection()
+            if (connection != null && connection!!.isConnected()) {
+                var reader: BufferedReader? = null
+                var writer: PrintWriter? = null
+                try {
+                    Log.e("tag", "connect-->" + connection?.isConnected())
+                    reader = BufferedReader(InputStreamReader(connection?.getInputStream()))
+                    writer = PrintWriter(OutputStreamWriter(connection?.getOutputStream()))
+                    writer.println("hello!")
+                    writer.flush()
+                    var line = reader.readLine()
+                    while (disconnect || line != "ok") {
+                        Log.e("tag", "server-->$line")
+                        writer.println("are you ok?")
+                        writer.flush()
+                        line = reader.readLine()//阻塞
+                    }
+                } catch (e: Exception) {
+                    Log.e("tag", "exception-->${e.message}" + e.printStackTrace())
+                } finally {
+                    reader?.close()
+                    writer?.flush()
+                    writer?.close()
+                    connection?.close()
+                }
+            }
         }.start()
+    }
+
+    override fun onDestroy() {
+        disconnect = true
+        super.onDestroy()
     }
 }
